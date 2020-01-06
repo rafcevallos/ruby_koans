02e377bf0c5c796be4cd6c06064446794f88aa34
Rafael Cevallos <rcevallo@RMTMACLT0045.local>
2020-01-06 15:39:20 -0600

    Finished about_array_assignment. Splat operators were a new thing to me.  Remember them from Python but NEVER thought to look up what they were.  Seems very useful and clarifies some confusion from my previous role.  Go figure.

--- koans/about_array_assignment.rb
+++ koans/about_array_assignment.rb
@@ -3,49 +3,57 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
     names = ["John", "Smith"]
-    assert_equal __, names
+    assert_equal ["John", "Smith"], names
   end
 
   def test_parallel_assignments
     first_name, last_name = ["John", "Smith"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_extra_values
     first_name, last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_splat_operator
     first_name, *last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal ["Smith", "III"], last_name
+    # Splat Operator
+    # - to handle an undefined number of arguments in ruby we have a parameter with the splat operator (*)
+    # - a parameter with the splat operator takes only those arguments for which there were no other parameters
+    # - a parameter with the splat operator is optional
+    # - a parameter with the splat operator the arguments to an array
+    # - if we do not pass any arguments for a parameter with the splat operator then a local variable within a method will reference to an empty array
+    # - the arguments are passed in the same order in which they are specified when a method is called.
+    # - a method can’t have two parameters with splat operator at the same time
   end
 
   def test_parallel_assignments_with_too_few_variables
     first_name, last_name = ["Cher"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "Cher", first_name
+    assert_equal nil, last_name
   end
 
   def test_parallel_assignments_with_subarrays
     first_name, last_name = [["Willie", "Rae"], "Johnson"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal ["Willie", "Rae"], first_name
+    assert_equal "Johnson", last_name
   end
 
   def test_parallel_assignment_with_one_variable
     first_name, = ["John", "Smith"]
-    assert_equal __, first_name
+    assert_equal "John", first_name
   end
 
   def test_swapping_with_parallel_assignment
     first_name = "Roy"
     last_name = "Rob"
     first_name, last_name = last_name, first_name
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "Rob", first_name
+    assert_equal "Roy", last_name
   end
 end
