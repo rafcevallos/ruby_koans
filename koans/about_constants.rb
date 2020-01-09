613203d8f1a1ed0b2a23381caf41b1ff0d0306e4
Rafael Cevallos <rcevallo@RMTMACLT0045.local>
2020-01-09 11:58:13 -0600

    Finished about_constants. Finally, learned what the < means in context of classes. Simply means inheritance.

--- koans/about_constants.rb
+++ koans/about_constants.rb
@@ -1,28 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-C = "top level"
+C = "top level" # capitalized variables are constants in Ruby
 
 class AboutConstants < Neo::Koan
 
   C = "nested"
 
   def test_nested_constants_may_also_be_referenced_with_relative_paths
-    assert_equal __, C
+    assert_equal "nested", C
   end
 
   def test_top_level_constants_are_referenced_by_double_colons
-    assert_equal __, ::C
+    assert_equal "top level", ::C
   end
 
   def test_nested_constants_are_referenced_by_their_complete_path
-    assert_equal __, AboutConstants::C
-    assert_equal __, ::AboutConstants::C
+    assert_equal "nested", AboutConstants::C
+    assert_equal "nested", ::AboutConstants::C
   end
 
   # ------------------------------------------------------------------
 
   class Animal
-    LEGS = 4
+    LEGS = 4 # constant
     def legs_in_animal
       LEGS
     end
@@ -35,19 +35,19 @@ class AboutConstants < Neo::Koan
   end
 
   def test_nested_classes_inherit_constants_from_enclosing_classes
-    assert_equal __, Animal::NestedAnimal.new.legs_in_nested_animal
+    assert_equal 4, Animal::NestedAnimal.new.legs_in_nested_animal
   end
 
   # ------------------------------------------------------------------
 
-  class Reptile < Animal
+  class Reptile < Animal # < indicates Reptile is inheriting Animal
     def legs_in_reptile
       LEGS
     end
   end
 
   def test_subclasses_inherit_constants_from_parent_classes
-    assert_equal __, Reptile.new.legs_in_reptile
+    assert_equal 4, Reptile.new.legs_in_reptile
   end
 
   # ------------------------------------------------------------------
@@ -63,11 +63,21 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_both_nested_and_inherited_constants
-    assert_equal __, MyAnimals::Bird.new.legs_in_bird
+    assert_equal 2, MyAnimals::Bird.new.legs_in_bird
   end
 
   # QUESTION: Which has precedence: The constant in the lexical scope,
   # or the constant from the inheritance hierarchy?
+  #  In this case, lexical scope wins
+
+  # Stack Overflow Link: https://stackoverflow.com/questions/4627735/ruby-explicit-scoping-on-a-class-definition
+  #   1. The enclosing scope
+  #   2. Any outer scopes (repeat until top level is reached) Any outer scopes (up to but not including the top level
+  #   3. Included modules
+  #   4. Superclass(es)
+  #   5. Top level
+  #   6. Object
+  #   7. Kernel
 
   # ------------------------------------------------------------------
 
@@ -78,10 +88,13 @@ class AboutConstants < Neo::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance hierarchy?  Why is it
   # different than the previous answer?
+
+  # Seems to be inheritance hierarchy in this case. LEGS isn't defined within the lexical scope, but
+  # the Oyster class is inheriting the Animal class where LEGS is set as 4
 end
